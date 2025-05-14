755dd1d0a31a502e5fb01c480caf72ab729622f3
Lordicode <84358705+lordicode@users.noreply.github.com>
2025-05-15 00:29:58 +0200

    Outline idea, set initial state of the grid

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -11,7 +11,29 @@ const double UPDATE_INTERVAL = 2.0;
 int main() {
     std::srand((std::time(nullptr)));
 
-    int gridW, gridH;
+    int gridInitialW, gridInitialH;
+
+    std::cout << "Enter the initial size of the grid width and height (max total cells "
+        << MAX_CELLS << "): ";
+    std::cin >> gridInitialW >> gridInitialH;
+    if (gridInitialW <= 0 || gridInitialH <= 0 || gridInitialW * gridInitialH > MAX_CELLS) {
+        std::cerr << "Error:grid height or width must be >0 and ≤"
+            << MAX_CELLS << "\n";
+        return 1;
+    }
+
+    std::vector<bool> grid(gridInitialW * gridInitialH);
+
+    for (int i = 0; i < gridInitialW * gridInitialH; ++i) {
+        grid[i] = (std::rand() % (4 - 1) + 1) == 1;
+    }
+
+    //error
+    //sf::RenderWindow window(sf::VideoMode(WINDOW_W, WINDOW_H), "Conway's Game of Life (SFML)");
+    //window.setFramerateLimit(60);
+
+    //TODO: start from initial grid of colored alide pixels and black dead pixels, dynamically zoom out to preserve scale of the game of life even with 100 000 cells.
+
 
     return 0;
 }
