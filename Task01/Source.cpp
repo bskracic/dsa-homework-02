bddd49dea5a2bf4ca0534025a9de7ae9b4cd0ae1
Stjepan Labor <stjepan.labor24@gmail.com>
2025-05-15 13:22:58 +0200

    DZ02

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+<<<<<<< HEAD
 
 int main()
 {
@@ -19,3 +20,26 @@ int main()
         window.display();
     }
 }
+=======
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "game_of_life.h"
+using namespace std;
+int main()
+{
+    
+        game_of_life the_game;
+
+        bool dalje;
+        do {
+            the_game.iscrtaj();
+            the_game.sljedeca_generacija();
+
+            cout << "Dalje (1/0): ";
+            cin >> dalje;
+        } while (dalje);
+
+        return 0;
+    }
+>>>>>>> 7833747 (DZ02)
