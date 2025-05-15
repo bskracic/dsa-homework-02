7336d11f9d9923720b55ecedd4fe2e5b27f0710f
Stjepan Labor <stjepan.labor24@gmail.com>
2025-05-15 13:38:31 +0200

    rijesen konflikt

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,26 +1,4 @@
-#include <SFML/Graphics.hpp>
-<<<<<<< HEAD
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-}
-=======
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -42,4 +20,4 @@ int main()
 
         return 0;
     }
->>>>>>> 7833747 (DZ02)
+
