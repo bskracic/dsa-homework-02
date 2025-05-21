e00e10346494f616975dfe0b6a0912b377c45941
Teo Kanducar <129656639+tkanducar@users.noreply.github.com>
2025-05-21 12:40:40 +0200

    first commit

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,21 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include "IgraZivota.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+	IgraZivota igra_zivota(&window, 800 / 8, 600 / 8, 8);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+	sf::Clock clock;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		while (const std::optional<sf::Event> event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		if (clock.getElapsedTime().asSeconds() > 2)
+
+			window.clear();
+		igra_zivota.draw();
+		window.display();
+		clock.restart();
+	}
+
+	return 0;
 }
